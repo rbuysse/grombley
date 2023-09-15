a3bcf1791bf0e0945950b6b5288149f062ca600e
Jared Miller <jared@smell.flowers>
2023-09-14 22:30:02 -0400

    Add chili dog url test

--- tests/scripts/test-image-upload.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-IMAGELOC=$( \
-   curl -s -D - -F 'file=@/tmp/test.jpg' grombley:3000/upload | \
-   grep Location | \
-   awk '{print $2}') \
-&& curl --fail-with-body -s -I "grombley:3000${IMAGELOC}"
