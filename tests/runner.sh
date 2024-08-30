80246df90d79f61349dacd6effa9e1b8f4ecc084
Ryan Beck-Buysse <rbuysse@gmail.com>
2024-08-29 19:05:27 -0500

    Delete grombley container before running tests
    Signed-off-by: Ryan Beck-Buysse <rbuysse@gmail.com>

--- tests/runner.sh
+++ tests/runner.sh
@@ -3,6 +3,10 @@
 set -e
 trap "docker-compose -f tests/docker-compose.yaml down > /dev/null 2>&1" EXIT
 
+if [ "$(docker ps -q -f name=grombley)" ]; then
+  docker kill grombley
+fi
+
 docker-compose -f tests/docker-compose.yaml build
 docker-compose -f tests/docker-compose.yaml up --detach grombley
 docker-compose -f tests/docker-compose.yaml up --detach nginx
